5950d04ba0ed5fc4c3ce74d7e3ba2e5d178598f2
Diego Magalhães <dgomesbr@gmail.com>
2015-05-06 18:32:56 -0300

    Tests corrected with line replaced and defined correctly

--- src/test/scala/com/diegomagalhaes/nginxlogparser/SampleCombinedAccessLogRecords.scala
+++ src/test/scala/com/diegomagalhaes/nginxlogparser/SampleCombinedAccessLogRecords.scala
@@ -12,11 +12,11 @@ object SampleCombinedAccessLogRecords {
 192.168.0.102 - - [04/May/2015:23:02:01 -0300]  "GET /adserver/www/delivery/lg.php?bannerid=9432&campaignid=681&zoneid=1637&loc=1&cb=fe8e6f00b0 HTTP/1.1" 0.000  43 "http://wap.tim.com.br/" Agent["Mozilla/5.0 (LG-T375 AppleWebkit/531 Browser/Phantom/V2.0 Widget/LGMW/3.0 MMS/LG-MMS-V1.0/1.2 Java/ASVM/1.1 Profile/MIDP-2.1 Configuration/CLDC-1.1)"] - . 200
 192.168.0.102 - - [04/May/2015:23:02:01 -0300]  "GET /adserver/www/delivery/lg.php?bannerid=9349&campaignid=676&zoneid=1555&loc=1&cb=91e0c8a58b HTTP/1.1" 0.000  43 "http://wap.tim.com.br/" Agent["Mozilla/5.0 (Linux; U; Android 4.1.2; pt-br; LG-E467f Build/JZO54K) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30"] - . 200
 192.168.0.102 - - [04/May/2015:23:02:01 -0300]  "GET /adserver/www/delivery/lg.php?bannerid=9382&campaignid=679&zoneid=1519&cb=40f1441eb8 HTTP/1.1" 0.000  43 "-" Agent["MOT-EX128 Obigo/WAP2.0 MIDP-2.0/CLDC-1.1"] - . 200
-""".split("\n").filter(_ != "")
+""".replaceAll("\r","").split("\n").filter(_ != "")
 
   val badRecord = """
 66.249.70.10 - - [23/Feb/2014:03:21:59 -0700] "GET /blog/post/java/how-load-multiple-spring-context-files-standalone/ HTTP/1.0" 301 - "-" "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
-""".split("\n").filter(_ != "")
+""".replaceAll("\r","").split("\n").filter(_ != "")
 
 
 }
